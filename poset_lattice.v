8292238740edbed1f40aca0a53ceea504faccfad
Avi Weinstock <aweinstock314@gmail.com>
2018-01-28 17:43:59 -0500

    Prove {bot,top}_prop for the product lattice.

--- poset_lattice.v
+++ poset_lattice.v
@@ -63,13 +63,27 @@ Definition PRODUCT_POSET (P1 P2 : POSET) : POSET := mkPoset (t P1 * t P2) (PRODU
     (PRODUCT_POSET_THEOREMS.refl P1 P2) (PRODUCT_POSET_THEOREMS.antisym P1 P2) (PRODUCT_POSET_THEOREMS.trans P1 P2).
 
 Module PRODUCT_LATTICE_THEOREMS.
+    Definition L' L1 L2 := PRODUCT_POSET (L L1) (L L2).
     Definition top L1 L2 := (top L1, top L2).
-    Definition bot L1 L2:= (bot L1, bot L2).
+    Definition bot L1 L2 := (bot L1, bot L2).
 
     Definition glb L1 L2 x y := (glb L1 (fst x) (fst y), glb L2 (snd x) (snd y)).
     Definition lub L1 L2 x y := (lub L1 (fst x) (fst y), lub L2 (snd x) (snd y)).
+
+    Ltac topbot L1 L2 which_prop which := 
+        simpl; specialize (which_prop L1); specialize (which_prop L2); intros H H0 x;
+        unfold which; unfold PRODUCT_POSET_THEOREMS.product_leq;
+        destruct x as [t0 t1]; simpl;
+        specialize (H0 t0); specialize (H t1);
+        rewrite H0; rewrite H;
+        reflexivity.
+
+    Theorem top_prop L1 L2 : forall x, leq (L' L1 L2) x (top L1 L2) = true. topbot L1 L2 top_prop top. Qed.
+    Theorem bot_prop L1 L2 : forall x, leq (L' L1 L2) (bot L1 L2) x = true. topbot L1 L2 bot_prop bot. Qed.
+
 End PRODUCT_LATTICE_THEOREMS.
 
+*)
 (*
 Definition PRODUCT_LATTICE (L1 L2 : LATTICE) : LATTICE := mkLattice (PRODUCT_POSET (L L1) (L L2))
     (PRODUCT_LATTICE_THEOREMS.top L1 L2) (PRODUCT_LATTICE_THEOREMS.bot L1 L2)
