006504f5cb22327e90489dc342850c872b643886
Avi Weinstock <aweinstock314@gmail.com>
2018-04-03 11:06:13 -0400

    Add Prop-based versions of some of the definitions in ListBackedSet.

--- ListBackedSet.v
+++ ListBackedSet.v
@@ -1,3 +1,12 @@
+Require Import Setoid.
+
+Lemma andb_idem : forall b, (b && b = b)%bool. destruct b; reflexivity. Qed.
+Lemma andb_assoc : forall a b c, ((a && b) && c = a && (b && c))%bool. destruct a,b,c; reflexivity. Qed.
+Lemma andb_prop' : forall a b : bool, a = true /\ b = true -> (a && b)%bool = true.
+    intros a b H. destruct a, b; easy. Qed.
+Lemma andb_prop_iff : forall a b : bool, (a = true /\ b = true) <-> ((a && b)%bool = true).
+    destruct a,b; tauto. Qed.
+
 Module ListBackedSet.
     (*Parameter A : Type.
     Parameter dec_eq : forall x y : A, {x = y} + {x <> y}.*)
@@ -65,5 +74,52 @@ Module ListBackedSet.
         - 
     *)
 
+    Theorem subset_prop_cons : forall A eq_dec (ys xs : list A) (x : A), subset A eq_dec (x :: xs) ys = true -> elem A eq_dec x ys = true /\ subset A eq_dec xs ys = true.
+        intros A eq_dec ys. induction ys; intros xs x H.
+        - discriminate H.
+        - unfold subset in H. apply andb_prop in H. fold subset in H. exact H.
+        Qed.
+
 End ListBackedSet.
 
+Module ListBackedSet'.
+    Inductive Elem {A} (x : A) : (list A) -> Prop := Elem_head : forall xs, Elem x (cons x xs) | Elem_tail : forall y xs, Elem x xs -> Elem x (cons y xs).
+    Definition Subset {A} (xs ys : list A) := forall x, Elem x xs -> Elem x ys.
+    Definition EqSet {A} (xs ys : list A) := Subset xs ys /\ Subset ys xs.
+
+    Theorem elem_compat : forall A eq_dec x xs, ListBackedSet.elem A eq_dec x xs = true <-> Elem x xs.
+        intros A eq_dec x xs. induction xs; [easy | split].
+        - destruct (eq_dec x a).
+            + intro H. rewrite e. apply Elem_head.
+            + rewrite (ListBackedSet.elem_cons_neq _ _ _ _ _ n).
+                intro H. exact (Elem_tail _ _ _ (proj1 IHxs H)).
+        - intro H. inversion H.
+           + apply ListBackedSet.elem_cons_eq; reflexivity.
+           + subst. exact (ListBackedSet.elem_cons_extend _ _ _ _ _ (proj2 IHxs H1)).
+        Qed.
+
+    Theorem Subset_lcontract {A} : forall x (xs ys : list A), Subset (x :: xs) ys -> Subset xs ys.
+        intros x xs ys H y H'. exact (H _ (Elem_tail _ _ _ H')). Qed.
+    Theorem Subset_rexpand {A} : forall y (xs ys : list A), Subset xs ys -> Subset xs (y :: ys).
+        intros y xs ys H x H'. exact (Elem_tail _ _ _ (H x H')). Qed.
+
+    Theorem subset_compat : forall A eq_dec xs ys, ListBackedSet.subset A eq_dec xs ys = true <-> Subset xs ys.
+        intros A eq_dec xs ys. split.
+        - induction xs.
+            + simpl. unfold Subset. intros H x H'. inversion H'.
+            + intros. unfold Subset. intros x H'.
+                apply andb_prop in H. fold ListBackedSet.subset in H. destruct H as [He Hs].
+                specialize (IHxs Hs x). apply elem_compat in He. destruct (eq_dec a x).
+                * subst; exact He.
+                * inversion H'; subst;  tauto.
+        - induction xs.
+            + reflexivity.
+            + intro H. apply andb_prop'. fold ListBackedSet.subset. split.
+                * exact (proj2 (elem_compat _ _ _ _) (H a (Elem_head _ _))).
+                * exact (IHxs (Subset_lcontract _ _ _ H)).
+        Qed.
+
+    Theorem eqset_compat : forall A eq_dec xs ys, ListBackedSet.eqset A eq_dec xs ys = true <-> EqSet xs ys.
+        intros A eq_dec xs ys. unfold ListBackedSet.eqset. rewrite <- andb_prop_iff. repeat rewrite subset_compat. reflexivity.
+        Qed.
+End ListBackedSet'.
