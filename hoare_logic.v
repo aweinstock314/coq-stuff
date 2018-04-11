2c5434fb017737696f11ae0e7db3116c1de8e807
Avi Weinstock <aweinstock314@gmail.com>
2018-04-11 13:49:50 -0400

    Add proper dynamic types to hoare_logic.v.

--- hoare_logic.v
+++ hoare_logic.v
@@ -1,44 +1,75 @@
+Load "ListBackedSet".
+
 (* adapted from http://www.cs.rpi.edu/~milanova/csci4450/Lecture19.pdf *)
-Inductive BinaryOperator := Plus | Times.
+Inductive LangType := TyNat | TyBool.
+Scheme Equality for LangType.
 
-Inductive Expr :=
-    | Lit : nat -> Expr
-    | Var : nat -> Expr
-    | Binop : BinaryOperator -> Expr -> Expr -> Expr
-    | EqualityComparison : Expr -> Expr -> Expr.
-Inductive Stmt :=
-    | Assign : nat -> Expr -> Stmt
-    | Seq : Stmt -> Stmt -> Stmt
-    | IfStmt : Expr -> Stmt -> Stmt -> Stmt
-    | WhileStmt : Expr -> Stmt -> Stmt
-    | Skip : Stmt.
-
-Definition Env := nat -> nat.
-
-Definition denote_binop (b : BinaryOperator) : (nat -> nat -> nat) := match b with
-    | Plus => Nat.add
-    | Times => Nat.mul
+Definition denote_type x := match x with
+    | TyNat => nat
+    | TyBool => bool
     end.
 
-Fixpoint denote_expr (sigma : Env) (e : Expr) : nat := match e with
-    | Lit e => e
-    | Var x => sigma x
-    | Binop b e1 e2 => denote_binop b (denote_expr sigma e1) (denote_expr sigma e2)
-    | EqualityComparison e1 e2 => match Nat.eqb (denote_expr sigma e1) (denote_expr sigma e2) with true => 1 | false => 0 end
+Inductive BinaryOperator : LangType -> LangType -> Type :=
+    Plus : BinaryOperator TyNat TyNat | Times : BinaryOperator TyNat TyNat |
+    And : BinaryOperator TyBool TyBool | Or : BinaryOperator TyBool TyBool |
+    Eq : forall T, DecEq (denote_type T) -> BinaryOperator T TyBool.
+
+Inductive Expr : LangType -> Type :=
+    | Lit : forall T (x:denote_type T), Expr T
+    | Var : forall T, nat -> Expr T
+    | Binop : forall T U, BinaryOperator T U -> Expr T -> Expr T -> Expr U.
+
+Inductive Stmt :=
+    | Assign : forall T, nat -> Expr T -> Stmt
+    | Seq : Stmt -> Stmt -> Stmt
+    | IfStmt : Expr TyBool -> Stmt -> Stmt -> Stmt
+    | WhileStmt : Expr TyBool -> Stmt -> Stmt
+    | Skip : Stmt.
+
+Definition Env := nat -> option {T : LangType & denote_type T}.
+Definition exampleEnv : Env := fun x => match x with
+    | 0 => Some (existT denote_type TyNat 5)
+    | 1 => Some (existT denote_type TyBool true)
+    | _ => None
     end.
 
 Definition bind_opt {A B} (x : option A) (f : A -> option B) : option B := match x with None => None | Some x' => f x' end.
 Infix ">>=" := bind_opt (at level 70).
-Definition augment_env (index : nat) (value : nat) (sigma : Env) : Env := fun n => match Nat.eqb n index with true => value | false => sigma n end.
+
+Definition null_env : Env := fun _ => None.
+Definition augment_env {T} (index : nat) (value : denote_type T) (sigma : Env) : Env :=
+    fun n => match Nat.eqb n index with
+        | true => Some (existT denote_type T value)
+        | false => sigma n
+    end.
+
+Definition denote_binop {T U} (b : BinaryOperator T U) : (denote_type T -> denote_type T -> denote_type U) := match b with
+    | Plus => Nat.add | Times => Nat.mul
+    | And => andb | Or => orb
+    | Eq _ eq_dec => fun x y => if eq_dec x y then true else false
+    end.
+
+Fixpoint denote_expr {T} (sigma : Env) (e : Expr T) : option (denote_type T) := match e with
+    | Lit _ e => Some e
+    | Var t1 x => sigma x >>= fun val => match val with existT _ t2 y =>
+        match LangType_eq_dec t1 t2 with
+        | left eq_t1_t2 => Some ltac:(rewrite eq_t1_t2; exact y)
+        | right _ => None
+        end end
+    | Binop _ _ b e1 e2 =>
+        (denote_expr sigma e1) >>= fun x =>
+        (denote_expr sigma e2) >>= fun y =>
+        Some (denote_binop b x y)
+    end.
 
 Fixpoint denote_stmt (depth : nat) (s : Stmt) (sigma : Env) : option Env := match depth with 0 => None | S n =>
     match s with
-    | Assign x e => Some (augment_env x (denote_expr sigma e) sigma)
+    | Assign _ x e => (denote_expr sigma e) >>= fun val => Some (augment_env x val sigma)
     | Seq s1 s2 => (denote_stmt n s1 sigma) >>= (fun sigma' => denote_stmt n s2 sigma')
-    | IfStmt e s1 s2 => denote_stmt n (match denote_expr sigma e with (S _) => s1 | 0 => s2 end) sigma
-    | WhileStmt e s => match denote_expr sigma e with
-        | (S _) => denote_stmt n s sigma >>= (fun sigma' => denote_stmt n (WhileStmt e s) sigma')
-        | 0 => Some sigma
+    | IfStmt e s1 s2 => denote_expr sigma e >>= fun val => denote_stmt n (if val then s1 else s2) sigma
+    | WhileStmt e s => denote_expr sigma e >>= fun val => match val with
+        | true => denote_stmt n s sigma >>= (fun sigma' => denote_stmt n (WhileStmt e s) sigma')
+        | false => Some sigma
         end
     | Skip => Some sigma
     end end.
