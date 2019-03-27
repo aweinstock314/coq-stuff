2df583a9627e8290eeea52a7a20acb1d39de671d
Avi Weinstock <aweinstock314@gmail.com>
2019-03-26 21:56:53 -0400

    Progress on embeding binders into LambdaCalculusALaCarte, with a PHOAS based attempt and an HList based attempt.

--- LambdaCalculusALaCarte.v
+++ LambdaCalculusALaCarte.v
@@ -10,6 +10,19 @@ Fixpoint spineDenotation (x : Spine) : Type := match x with
     | Partial a b => a -> spineDenotation b
     end.
 
+Fixpoint spineNonLast (x : Spine) : Type := match x with
+    | Full a => unit
+    | Partial a b => prod a (spineNonLast b)
+    end.
+
+Fixpoint spineLast (x : Spine) : Type := match x with
+    | Full a => a
+    | Partial a b => spineLast b
+    end.
+
+Fixpoint varUncurry (x : Spine) (f : spineDenotation x) (xs : spineNonLast x) {struct x} : spineLast x := ltac:(destruct x; [exact f | exact (varUncurry x (f (fst xs)) (snd xs))]).
+Fixpoint varCurry (x : Spine) (f : spineNonLast x -> spineLast x) {struct x} : spineDenotation x := ltac:(destruct x; [exact (f tt) | exact (fun y => varCurry _ (fun z => f (y, z)))]).
+
 Infix ":->" := Partial (at level 90, right associativity).
 
 Inductive AST dom (sig : Spine) :=
@@ -31,7 +44,7 @@ Inductive TyPlus (dom1 dom2 : Spine -> Type) a :=
 Arguments InjL {_ _ _} _.
 Arguments InjR {_ _ _} _.
 
-Infix ":+:" := TyPlus (at level 90).
+Infix ":+:" := TyPlus (at level 90, right associativity).
 
 Class Subtype (sub sup : Spine -> Type) := {
     inj : forall a, sub a -> sup a;
@@ -96,3 +109,87 @@ Instance BSE_IfExpr : BigStepEval IfExpr := { big_step_eval a x := match x with 
 Definition not' {dom} `(_ : BooleanSym :<: dom) : ASTF dom bool -> ASTF dom bool := fun x => inj (BoolUnop negb) :$ x.
 
 Definition demo_exp_1 : ASTF (IfExpr :+: BooleanSym) bool := inj (IfThenElse _) :$ (inj (BoolLit true)) :$ (inj (BoolLit false)) :$ (inj (BoolLit true)).
+
+Inductive SimpLam V W : Spine -> Type :=
+    | Var : forall {a}, V a -> SimpLam V W (Full a)
+    | Abs : forall {a b}, (V a -> W b) -> SimpLam V W (a :-> b).
+Definition SimpLam' W sig := forall V, SimpLam V W sig.
+
+Instance BSE_SimpLam : BigStepEval (SimpLam' spineDenotation) := {
+    big_step_eval s x := let x' := x (fun x => x) in
+    match x' with Var _ _ y => y | Abs _ _ f => f end
+    }.
+Instance BSE_SimpLam' {dom} `{_ : BigStepEval dom} : BigStepEval (SimpLam' (AST dom)) := {
+    big_step_eval _ x := let x' := x (fun x => x) in
+    match x' with Var _ _ y => y | Abs _ _ f => fun y => big_step_eval (f y) end
+    }.
+
+Inductive NatSym : Spine -> Type := Zero : NatSym (Full nat) | Succ : NatSym (nat :-> Full nat).
+Instance BSE_NatSym : BigStepEval NatSym := { big_step_eval _ x := match x with Zero => O | Succ => S end }.
+
+Inductive PR dom : Spine -> Type := Rec : forall s, PR dom (AST dom s :-> AST dom (nat :-> spineLast s :-> s) :-> (nat :-> s)).
+Instance BSE_PR dom `{_ : BigStepEval dom} : BigStepEval (PR dom) := {
+    big_step_eval a x := match x in PR _ s return spineDenotation s with Rec _ s =>
+        fun f g => fix h n := match n with
+            | O => big_step_eval f
+            | S n' => varCurry s (fun y => varUncurry s (big_step_eval g n' (varUncurry s (h n') y)) y)
+        end
+    end
+    }.
+
+Definition ast_fst {dom} {V} `{_ : SimpLam V (AST dom) :<: dom} {a b} : AST dom (a :-> b :-> Full a) := ltac:(apply Sym, inj, Abs; intro x; apply Sym, inj, Abs; intro y; apply Sym, inj, Var; exact x).
+Fail Definition ast_fst' {dom} {V} `{_ : SimpLam V spineDenotation :<: dom} {a b} : AST dom (a :-> b :-> Full a) := ltac:(apply Sym, inj, Abs; exact (fun x y => x)). (* nonparametric *)
+
+Inductive VoidSym : Spine -> Type := .
+
+Fail Check (@App _ _ _ (Sym (Rec _ (nat :-> Full nat))) _).
+Definition ast_plus  {V} : AST (PR VoidSym :+: (SimpLam V (AST VoidSym) :+: NatSym)) (nat :-> nat :-> Full nat).
+refine (_ :$ _ :$ _).
+- apply Sym; left.
+    Fail apply Rec.
+Abort.
+
+Inductive hlist :=
+    | hnil : hlist
+    | hcons : forall A, A -> hlist -> hlist
+    .
+
+Inductive hElem {A} (x : A) : hlist -> Prop :=
+    | hElemHead : forall xs, hElem x (hcons _ x xs)
+    | hElemTail : forall {B} (y : B) xs, hElem x xs -> hElem x (hcons _ y xs)
+    .
+
+Class Search_hElem {A} (x : A) (xs : hlist) := { searchHlist : hElem x xs }.
+Instance Search_hElem_Head {A} x xs : Search_hElem x (hcons A x xs) := ltac:(constructor; constructor).
+Instance Search_hElem_Tail {A B} {y : B} (x : A) xs `{H : Search_hElem A x xs} : Search_hElem x (hcons B y xs) := ltac:(constructor; apply hElemTail; destruct H; assumption).
+
+Inductive ContextedLam : hlist -> Spine -> Type :=
+    | CtxVar : forall {A} (x : A) {xs}, hElem x xs -> ContextedLam xs (Full A)
+    | CtxAbs : forall {A B xs}, (forall x, ContextedLam (hcons A x xs) (Full B)) -> ContextedLam xs (Full (A -> B))
+    .
+
+Instance BSE_ContextedLam (ctx : hlist) : BigStepEval (ContextedLam ctx) := ltac:(constructor; induction 1; assumption).
+
+Definition embed_snd {dom} `{_ : ContextedLam hnil :<: dom} a b : AST dom (Full (a -> b -> b)) := inj (CtxAbs (fun _ => CtxAbs (fun y => CtxVar y searchHlist))).
+
+Compute big_step_eval (embed_snd nat nat).
+
+Inductive PR' : Spine -> Type := Rec' : forall s, PR' (spineDenotation s :-> (nat -> spineLast s -> spineDenotation s) :-> Full (nat -> spineDenotation s)).
+Instance BSE_PR' : BigStepEval PR' := {
+    big_step_eval a x := _
+    }.
+(destruct x).
+(induction s; simpl; intros).
+- (induction H).
+    + exact X.
+    + exact (X0 H IHnat).
+- refine (IHs (X X1) _ H).
+    intros.
+    exact (X0 H0 X2 X1).
+Defined.
+
+Definition PR_plus : AST (PR' :+: NatSym :+: ContextedLam hnil) (Full (nat -> nat -> nat)).
+refine ((inj (Rec' (nat :-> Full nat)) : AST _ ((nat -> nat) :-> (nat -> nat -> nat -> nat) :-> Full (nat -> nat -> nat))) :$ _ :$ _).
+- exact (inj (CtxAbs (fun x => CtxVar x searchHlist))).
+- exact (inj (CtxAbs (fun x => CtxAbs (fun y => (CtxAbs (fun z => CtxVar x searchHlist)))))). (* this isn't yet correct, the shape of ContextedLam doesn't allow splicing in other domain elements, so we can't use Succ *)
+Defined.
